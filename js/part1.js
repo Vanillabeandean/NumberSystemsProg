bb3627d9db70b911173163e94a94ca336757523c
Vanillabeandean <deanbarkin@gmail.com>
2020-01-21 23:04:14 -0800

    Got part 1 working but not 2 or 3

--- js/part1.js
+++ js/part1.js
@@ -1,14 +1,96 @@
 function ConversionPart1() {
 
-    var UnsignedInt = parseInt(document.getElementById("1_UnsignedInt").value);
-    var UnsignedIntBaseFrom = parseInt(document.getElementById("1_UnsignedIntBaseToConvertFrom").value);
-    var UnsignedIntBaseTo = parseInt(document.getElementById("1_UnsignedIntBaseToConvertTo").value);
+    var UnsignedInt = parseInt(document.getElementById("1_UnsignedInt").value);
+    var UnsignedIntBaseFrom = parseInt(document.getElementById("1_UnsignedIntBaseToConvertFrom").value);
+    var UnsignedIntBaseTo = parseInt(document.getElementById("1_UnsignedIntBaseToConvertTo").value);
 
+    var Int2 = document.getElementById("1_UnsignedInt").value;
 
+      var c = 0;
+      while (Int2 != "") {
+        c = c * UnsignedIntBaseFrom + parseChar(Int2.charAt(0));
+        Int2 = Int2.substring(1, UnsignedInt.length);
+      }
 
-  var outputValue = "11111111";
+      var out = "";
 
-  // Show the output on the screen
-  FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputValue], 1);
+      while (c != 0) {
+        out = assignChar(c % UnsignedIntBaseTo) + out;
+        c = Math.trunc(c / UnsignedIntBaseTo);
+      }
 
+      // Show the output on the screen
+      FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, out], 1);
+
+function parseChar(thing) {
+  switch (thing) {
+    case "a":
+      return 10;
+      break;
+    case "b":
+      return 11;
+      break;
+    case "c":
+      return 12;
+      break;
+    case "d":
+      return 13;
+      break;
+    case "e":
+      return 14;
+      break;
+    case "f":
+      return 15;
+      break;
+    case "A":
+      return 10;
+      break;
+    case "B":
+      return 11;
+      break;
+    case "C":
+      return 12;
+      break;
+    case "D":
+      return 13;
+      break;
+    case "E":
+      return 14;
+      break;
+    case "F":
+      return 15;
+      break;
+    default:
+      return parseInt(thing);
+      break;
+  }
 }
+
+function assignChar(thing) {
+  switch (thing) {
+    case 10:
+        return "A";
+      break;
+    case 11:
+        return "B";
+      break;
+    case 12:
+        return "C";
+      break;
+    case 13:
+        return "D";
+      break;
+    case 14:
+        return "E";
+      break;
+    case 15:
+        return "F";
+      break;
+    default:
+      return "" + thing;
+      break;
+
+  }
+}
+
+    }
--- js/part3.js
+++ js/part3.js
@@ -1,26 +1,86 @@
-
 function ConversionPart3() {
   var floatToConvert = parseFloat(document.getElementById("3_Float").value);
+  var negative = floatToConvert<0;
+  var leftHalf = Math.abs(Math.trunc(floatToConvert));
+  var rightHalf = Math.abs(floatToConvert % 1);
+  var mantessa;
+  var negativeExp = false;
 
+  var leftDec = "";
+  while (leftHalf != 0) {
+    leftDec = assignChar(leftHalf % 2) + leftDec;
+    leftHalf = Math.trunc(leftHalf / 2);
+  }
 
+  var rightDec = "";
+  while (rightHalf!=0){
+    rightHalf= rightHalf*2;
+    rightDec += Math.trunc(rightHalf);
+    rightHalf = rightHalf % 1;
+  }
 
-  var output32BitScientificNotation = "10100011001100001000010100101010";
+  if (leftDec!=0){
+    var exp = leftDec.length -1;
+    leftDec = "" + leftDec;
+    mantessa = leftDec.substring(1)+ rightDec;
+  }
+  else{
+    var c =0;
+    while (rightDec.charAt(c)!=1){
+      c++;
+    }
+    var exp = -c -1;
+    mantessa = rightDec.substring(c+1);
+  }
 
+  while (mantessa.length<23){
+    mantessa+="0";
+  }
+  mantessa = mantessa.substring(0,24);
 
+  exp += 128;
+  if (exp<0){
+    negativeExp = true;
+    exp = Math.abs(exp);
+  }
+  var binaryExp="";
+  while (exp!=0){
+    binaryExp = (exp % 2) + binaryExp;
+    exp = Math.trunc(exp/2);
+  }
+  while (binaryExp.length<8){
+    binaryExp = "0" + binaryExp;
+  }
+  if (negativeExp){
+    var newBinExp;
+    for (var i =0; i< binaryExp.length; i++){
+      if (binaryExp.charAt(i)==0){
+        newBinExp += 1;
+      }
+      else {
+        newBinExp += 0;
+      }
+    }
+
+    for (var i = newBinExp.length -1; i>=0 ; i--){
+      if (newBinExp.charAt(i)=="0"){
+        newBinExp= newBinExp.substring(0, i)+"1"+newBinExp.substring(i+1);
+        i = -10;
+      }
+      else{
+        newBinExp= newBinExp.substring(0, i)+"0"+newBinExp.substring(i+1);
+       }
+    }
+    binaryExp = newBinExp;
+  }
+
+  if (negative){
+    negative = "1";
+  }
+  else{
+    negative = "0";
+  }
+  var output32BitScientificNotation = mantessa + binaryExp + negative;
   // Show the output on the screen
   FormatAndShowOutput([floatToConvert, output32BitScientificNotation], 3);
 }
-
-
-// If you dare read a comment before starting to program..
-// 3434000.5 has a binary representation of
-//  1101000110011000010000.1
-// In NORMALIZED scientific notation (i.e. scientific notation for Base 2)
-// 1.1010001100110000100001 * 2^21
-// ... so mantissa is 11010001100110000100001
-
-// For the final 32 bits.. we have
-// ... so 1010001100110000100001 for mantissa (because of explicit leading 1)
-// ... so for bits (0-22) 10100011001100001000010
-// ... so exponent representation in +128 format is 21+128 = 149 = (bits 23-30) 10010101
-// ... so final sign bit = (bit 31) 0
